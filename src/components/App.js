4ae18d3bc9c02fc965a30f2869ece36925dc8a2b
Norbert Korczyk <korczyk.norbert@gmail.com>
2020-09-12 10:27:55 +0200

    fetching data

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import News from './News/News';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>My feed</h1>
       </header>
+      <News></News>
     </div>
   );
 }
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import NewSingle from "../NewSingle/NewSingle";
+import React, { useEffect, useState } from 'react';
+import NewSingle from '../NewSingle/NewSingle';
 
-const News = ({ items }) => {
+const News = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const url =
+      'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=33af02ae673140ccab96f444c210a391';
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setNews(data.articles))
+      .catch(error => console.log('error: ', error));
+  }, []);
+
+  console.log('News', news);
+
   const renderItem = () =>
-    items.map(({ item }) => <NewSingle key={item.id} item={item}></NewSingle>);
+    news.map(item => <NewSingle key={item.publishedAt} item={item}></NewSingle>);
 
   return <ul>{renderItem()}</ul>;
 };
